8957907b8b6f9b97a940f3ef4b0b78c4bd4cb444
marcioa1 <marcioa1@gmail.com>
2019-10-21 11:22:23 -0300

    first commit and verifying uploaded file

---
+++ app/controllers/uploads_controller.rb
@@ -0,0 +1,13 @@
+class UploadsController < ApplicationController
+  def new
+  end
+
+  def parse
+    uploaded_file = params[:file]
+    if uploaded_file.present? && uploaded_file.original_filename == 'CNAB.txt'
+    else
+      flash[:error] = "Escolha o arquivo com nome CNAB.txt"
+      render 'new'
+    end
+  end
+end
